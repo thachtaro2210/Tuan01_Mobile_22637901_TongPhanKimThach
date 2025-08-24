6260e5d8e28cde3adc7eb5a9d693ff8dd4036df0
Tống Phan Kim Thạch <22637901.thach@student.iuh.edu.vn>
2025-08-24 21:09:57 +0700

    Complete Bai17fix the error at bai24

---
+++ src/BaiTap/bai24.ts
@@ -0,0 +1,17 @@
+abstract class Appliance {
+  abstract turnOn(): void;
+}
+
+
+class Fan extends Appliance {
+  turnOn(): void {
+    console.log("Fan is now ON ");
+  }
+}
+
+
+class AirConditioner extends Appliance {
+  turnOn(): void {
+    console.log("Air Conditioner is now ON ");
+  }
+}
